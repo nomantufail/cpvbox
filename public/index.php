3d9c6f710c9a4a5944a4d63c9c44d83f2c8eb342
nomantufail <nomantufail100@gmail.com>
2017-03-20 12:11:46 +0500

    comited

--- public/index.php
+++ public/index.php
@@ -517,7 +517,6 @@
         </div>
     </footer>
 
-
     <!-- Scripts -->
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
